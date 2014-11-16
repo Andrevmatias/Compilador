406616d1cfe1fe21f3b962218778a5e179f1f3f4
gsoaresc <gabrielsoarescosta.gsc@gmail.com>
2014-11-16 05:19:25 -0200

    Atualizado com última versão do léxico+sintático

--- src/br/edu/ufsc/compilador/analisadores/Constants.java
+++ src/br/edu/ufsc/compilador/analisadores/Constants.java
@@ -1,56 +1,63 @@
 package br.edu.ufsc.compilador.analisadores;
 
-public interface Constants extends ScannerConstants
+public interface Constants extends ScannerConstants, ParserConstants
 {
     int EPSILON  = 0;
     int DOLLAR   = 1;
 
     int t_id = 2;
-    int t_c_inteiro = 3;
-    int t_c_real = 4;
-    int t_c_cadeia = 5;
+    int t_num_int = 3;
+    int t_num_real = 4;
+    int t_literal = 5;
     int t_var = 6;
     int t_caracter = 7;
     int t_cadeia = 8;
-    int t_funcao = 9;
-    int t_inicio = 10;
-    int t_fim = 11;
-    int t_inteiro = 12;
-    int t_booleano = 13;
-    int t_se = 14;
-    int t_entao = 15;
-    int t_senao = 16;
-    int t_leia = 17;
-    int t_escreva = 18;
-    int t_ou = 19;
-    int t_e = 20;
-    int t_nao = 21;
-    int t_falso = 22;
-    int t_verdadeiro = 23;
-    int t_de = 24;
-    int t_faca = 25;
-    int t_real = 26;
-    int t_vetor = 27;
-    int t_enquanto = 28;
-    int t_TOKEN_29 = 29; //";"
-    int t_TOKEN_30 = 30; //","
-    int t_TOKEN_31 = 31; //"."
-    int t_TOKEN_32 = 32; //">"
-    int t_TOKEN_33 = 33; //"<"
-    int t_TOKEN_34 = 34; //"="
-    int t_TOKEN_35 = 35; //"("
-    int t_TOKEN_36 = 36; //")"
-    int t_TOKEN_37 = 37; //"["
-    int t_TOKEN_38 = 38; //"]"
-    int t_TOKEN_39 = 39; //"+"
-    int t_TOKEN_40 = 40; //"-"
-    int t_TOKEN_41 = 41; //"*"
-    int t_TOKEN_42 = 42; //"/"
-    int t_TOKEN_43 = 43; //":"
-    int t_TOKEN_44 = 44; //":="
-    int t_TOKEN_45 = 45; //".."
-    int t_TOKEN_46 = 46; //"<>"
-    int t_TOKEN_47 = 47; //"<="
-    int t_TOKEN_48 = 48; //">="
+    int t_inteiro = 9;
+    int t_booleano = 10;
+    int t_se = 11;
+    int t_entao = 12;
+    int t_senao = 13;
+    int t_leia = 14;
+    int t_escreva = 15;
+    int t_ou = 16;
+    int t_e = 17;
+    int t_nao = 18;
+    int t_falso = 19;
+    int t_verdadeiro = 20;
+    int t_de = 21;
+    int t_faca = 22;
+    int t_real = 23;
+    int t_vetor = 24;
+    int t_enquanto = 25;
+    int t_programa = 26;
+    int t_const = 27;
+    int t_metodo = 28;
+    int t_ref = 29;
+    int t_val = 30;
+    int t_registro = 31;
+    int t_retorne = 32;
+    int t_div = 33;
+    int t_TOKEN_34 = 34; //";"
+    int t_TOKEN_35 = 35; //","
+    int t_TOKEN_36 = 36; //"."
+    int t_TOKEN_37 = 37; //">"
+    int t_TOKEN_38 = 38; //"<"
+    int t_TOKEN_39 = 39; //"="
+    int t_TOKEN_40 = 40; //"("
+    int t_TOKEN_41 = 41; //")"
+    int t_TOKEN_42 = 42; //"["
+    int t_TOKEN_43 = 43; //"]"
+    int t_TOKEN_44 = 44; //"+"
+    int t_TOKEN_45 = 45; //"-"
+    int t_TOKEN_46 = 46; //"*"
+    int t_TOKEN_47 = 47; //"/"
+    int t_TOKEN_48 = 48; //":"
+    int t_TOKEN_49 = 49; //":="
+    int t_TOKEN_50 = 50; //".."
+    int t_TOKEN_51 = 51; //"<>"
+    int t_TOKEN_52 = 52; //"<="
+    int t_TOKEN_53 = 53; //">="
+    int t_TOKEN_54 = 54; //"{"
+    int t_TOKEN_55 = 55; //"}"
 
 }
