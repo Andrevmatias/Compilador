1b0c3f74c84741053b225e86fca0c500a1a22c7b
gsoaresc <gabrielsoarescosta.gsc@gmail.com>
2014-12-04 17:59:53 -0200

    Tentativa de começar as regras semanticas

--- src/br/edu/ufsc/compilador/analisadores/Semantico.java
+++ src/br/edu/ufsc/compilador/analisadores/Semantico.java
@@ -1,9 +1,40 @@
 package br.edu.ufsc.compilador.analisadores;
 
+import br.edu.ufsc.compilador.analisadores.semantico.identificadores.*;
+import br.edu.ufsc.compilador.analisadores.semantico.*;
+
 public class Semantico implements Constants
 {
-    public void executeAction(int action, Token token)	throws SemanticError
+	TabelaDeSimbolos tabela = new TabelaDeSimbolos();
+	int nivelAtual = 0;
+	
+    public void executeAction(int action, Token token)	throws SemanticError, IdentificadorJaDefinidoException
     {
-        System.out.println("A��o #"+action+", Token: "+token);
+    	switch(action)
+    	{
+    		case 101: System.out.println("ENTROU #101");
+    		String nomePrograma = token.getLexeme();
+    		IdentificadorPrograma idPrograma = new IdentificadorPrograma(nomePrograma);
+    		tabela.add(idPrograma, nivelAtual);
+    		System.out.println("FIM #101");
+    		break;
+    		
+    		case 102: System.out.println("ENTROU #102");
+    		String nomeConstante = token.getLexeme();
+    		IdentificadorConstante idConstante = new IdentificadorConstante(nomeConstante, null, null);
+    		tabela.add(idConstante, nivelAtual);
+    		//Falta guardar posição na tabela(necessário?)
+    		System.out.println("FIM #102");
+    		break;
+    		
+    		case 103: System.out.println("ENTROU #103");
+    		//Atualiza os atributos 'null' da constante de #102 na tabela
+    		//Eu acredito que basta atualizar 'idConstante'
+    		System.out.println("FIM #103");
+    		break;
+    		
+    		default: System.out.println("Ação #"+action+", Token: "+token); 
+        }
+
     }	
 }
